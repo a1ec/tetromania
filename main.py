379720158b2f96771dfb7ed736f9dfeb52cc571a
Israel Folau <israel@folau.com>
2023-11-01 09:19:15 +1100

    Add screen draw and keyboard input handling

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import pygame
+import config
+import colors
 
+pygame.init()
+screen = pygame.display.set_mode(config.SCREEN_PIXEL_DIMENSIONS)
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                break
+
+    screen.fill(colors.DARK_BLUE)
+    pygame.display.update()
+    clock.tick(config.SCREEN_UPDATE_HERTZ)
